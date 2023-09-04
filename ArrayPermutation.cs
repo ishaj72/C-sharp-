99190bcdc407dd5d7f3deda3a3fabc86ac334a2a
Isha Jain <75637232+ishaj72@users.noreply.github.com>
2023-09-04 21:28:23 +0530

    Create ArrayPermutation.cs

---
+++ ArrayPermutation.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int[] BuildArray(int[] nums) {
+        int n = nums.Length;
+        int[] ans = new int[n]; 
+
+        for(int i = 0 ; i < n ; i ++){
+            ans[i] = nums[nums[i]];
+        }
+
+        return ans;
+    }
+}
